92a907b2c1ed193dc39046b252af9cb88fc1fbd8
shash <shashuec@gmail.com>
2022-01-31 12:16:56 +0530

    Hello World

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        Fast Feedback
+          </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
